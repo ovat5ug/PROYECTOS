0bcaa196f56fd1859727031e40ea24b296b61aac
Οβατσυγ Περεζ <yahoo_3257@hotmail.com>
2025-03-20 16:25:53 -0600

    login actualizacion 02
    se comenzo a desarrollar la parte de login, para la validacion del mismo

--- USER/login.py
+++ USER/login.py
@@ -1,22 +1,16 @@
 import users
-
-class Login(users.Users):    
-    def __init__(self, email, password):
-        super().__init__(email, password)    
-
-    def tipoDeAcceso(self, type_of_user="regular"):
-        self.type_of_user = type_of_user
-        print(f"ha iniciado Sesion: {self.email}")
-        return self
+import login_users
 
 # Datos de prueba
 nombre, fecha_actual, hora_actual, tipo_de_usuario = "usuario1", '2025-03-18', '15:56:30', 'Regular'
-email, password = "marcos@qwerty.com", "qwasdefrg45wqxx-.,,"
+email, password = input("Ingrese su correo "), input("Ingrese si contraseña: ")
 
 # Creando instancia
 usuario = users.Users(email, password) 
 
-marcos = Login(usuario.email,usuario.password)
-marcos.tipoDeAcceso(tipo_de_usuario)
+login = login_users.Login(usuario.email,usuario.password)
+login.tipoDeAcceso(tipo_de_usuario)
+
+# print(marcos)  # Para ver la representación del objeto
 
 # if (correo=CVcorreo): VALIDACION
---
+++ USER/login_users.py
@@ -0,0 +1,10 @@
+import users
+
+class Login(users.Users):    
+    def __init__(self, email, password):
+        super().__init__(email, password)    
+
+    def tipoDeAcceso(self, type_of_user="regular"):
+        self.type_of_user = type_of_user
+        print(f"ha iniciado Sesion: {self.email}")
+        return self
