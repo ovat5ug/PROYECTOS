4700a6d49a2aca76020c26ee77311df8cf63fe24
Οβατσυγ Περεζ <yahoo_3257@hotmail.com>
2025-03-21 14:40:32 -0600

    cambios en registro y crear archivo
    los modulos de registro y crear archivo sufrieron pequeños cambios, para poder integarles validacion de uso y asi poder utilizar estas para la seleccion en el login de usuario

--- USER/CREAR_DOCUMENTACION/crear_archivo.py
+++ USER/CREAR_DOCUMENTACION/crear_archivo.py
@@ -3,17 +3,19 @@ import csv
 import importar_data
 import registrar
 
-def docCreado():
-    func_registrar = registrar.registrar()
+def docCreado(validar_cuantos_datos=True):
+    func_registrar = registrar.registrar(validar_cuantos_datos)
     lista_datos = func_registrar[0]# Retorna la primera lista de la tupla (datos del usuario)
     nombre_del_archivo = input( "ingrese el nombre para el archivo: ")
-    if __name__ == "__main__":
+    nCarpeta="DATA" # Se guarda en la raiz de la carpeta en donde se ejecuta
+    if (__name__ == "__main__" or __name__ != True): # o solo poner True como condicion
         # Definiendo la ruta del archivo
-        ruta_archivo = f"DATA\\{nombre_del_archivo}.csv"       
+        ruta_archivo = f"{nCarpeta}\\{nombre_del_archivo}.csv"       
         # Definiendo los encabezados
         encabezados = ["Usuario", "Email", "Password", "Tipo_de_Usuario","Fecha","Hora"]    
         # invocando a la funcion
         importar_data.creaando_csv_si_no_existe(ruta_archivo, encabezados, lista_datos)
-        
+
+# poner "(false)" para iniciar solamente una vez el registro de datos        
 # documento = docCreado() # TEST
 # documento
--- USER/CREAR_DOCUMENTACION/registrar.py
+++ USER/CREAR_DOCUMENTACION/registrar.py
@@ -2,9 +2,12 @@ from datetime import datetime  # datetime
 from USER import users
 from USER import contrasena
 
-def registrar():
+def registrar(validar_cuantos_datos=True):
     lista_datos=[]
-    cuantos_datos = int(input("ingrese la cantidad de datos que ingresara: "))
+    if validar_cuantos_datos:
+        cuantos_datos = int(input("ingrese la cantidad de datos que ingresara: "))
+    else:
+        cuantos_datos = 1  # Solo se registrara un solo usuario por login 
     for i in range(cuantos_datos):
         nombre=input("ingrese su nombre: ").title()
         correo=input("ingrese su correo: ")
@@ -13,14 +16,23 @@ def registrar():
         fecha = datetime.now()# Obteniendo Fecha Actual
         fecha_actual = fecha.strftime("%Y-%m-%d")
         hora_actual = fecha.strftime("%H:%M:%S")    
+        # sUsuario=users.Users(correo,contrasena.contrasena_encriptada())
         tUsuario = users.TypeOfUsers(nombre, correo, c_encriptada, fecha_actual, hora_actual, tipo_de_usuario)
+        # print("hola mundo") #TEST
         lista_datos.append([tUsuario.user,tUsuario.email,tUsuario.password, tUsuario.type_of_user,tUsuario.date,tUsuario.hour])
     return lista_datos,contrasena.desencriptar_clave.desencriptar(c_encriptada) 
 
 # func_registrar = registrar()
-# lista_datos = func_registrar[0]# Retorna la primera lista de la tupla
-# c_desencriptada = func_registrar[1] #Retorna la contraseña
 
-# print(f"esta es la lista de datos: \n{lista_datos}")# TEST
-# print()
+# c_desencriptada = func_registrar[1] #Retorna la contraseña
 # print(f"esta es la contraseña: {c_desencriptada}")# TEST
+
+# lista_datos# = ["Usuario", "Email", "Password", "Tipo_de_Usuario","Fecha","Hora"] 
+# lista_datos = func_registrar[0]# Retorna la primera lista de la tupla
+# lista_datos1 = func_registrar[0][0][:]
+# lista_datos2 = ["Email", "Password", "Fecha","Hora"] 
+# lista_datos2 = lista_datos4[1],lista_datos4[2],lista_datos4[4],lista_datos4[5]           
+# print(f"esta es la lista de datos: \n{lista_datos}")# TEST
+# print(f"esta es la lista de datos1: \n{lista_datos2}")# TEST
+# print(f"esta es la lista de focalizada: \n{lista_datos3}")# TEST
+# # print()
