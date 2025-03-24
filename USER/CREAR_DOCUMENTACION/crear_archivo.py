9a6d633fa164a6db3d86211c1ead80a908847358
Οβατσυγ Περεζ <yahoo_3257@hotmail.com>
2025-03-23 18:36:54 -0600

    creacion de excepciones para "registros de usuarios"
    el modulo "registrar" y "crear_archivos" ahora validan si el usuario ingresa un dato valio, ingresa solamente un enter o si solo pone espacios. para evitar que el programa se detenga y continue su flujo

--- USER/CREAR_DOCUMENTACION/crear_archivo.py
+++ USER/CREAR_DOCUMENTACION/crear_archivo.py
@@ -8,13 +8,16 @@ def docCreado(validar_cuantos_datos=True):
     lista_datos = func_registrar[0]# Retorna la primera lista de la tupla (datos del usuario)
     nombre_del_archivo = input( "ingrese el nombre para el archivo: ")
     nCarpeta="DATA" # Se guarda en la raiz de la carpeta en donde se ejecuta
-    if (__name__ == "__main__" or __name__ != True): # o solo poner True como condicion
-        # Definiendo la ruta del archivo
-        ruta_archivo = f"{nCarpeta}\\{nombre_del_archivo}.csv"       
-        # Definiendo los encabezados
-        encabezados = ["Usuario", "Email", "Password", "Tipo_de_Usuario","Fecha","Hora"]    
-        # invocando a la funcion
-        importar_data.creaando_csv_si_no_existe(ruta_archivo, encabezados, lista_datos)
+    if (__name__ == "__main__" or __name__ != True): # o solo poner True como condicion        
+        if nombre_del_archivo.strip():# Verifica que no esté vacío ni solo contenga espacios
+            # Definiendo la ruta del archivo
+            ruta_archivo = f"{nCarpeta}\\{nombre_del_archivo}.csv"       
+            # Definiendo los encabezados
+            encabezados = ["Usuario", "Email", "Password", "Tipo_de_Usuario","Fecha","Hora"]    
+            # invocando a la funcion
+            importar_data.creaando_csv_si_no_existe(ruta_archivo, encabezados, lista_datos)
+        else:
+            print("No se creó el archivo porque no ingresaste un nombre válido.")
 
 # poner "(false)" para iniciar solamente una vez el registro de datos        
 # documento = docCreado() # TEST
--- USER/CREAR_DOCUMENTACION/registrar.py
+++ USER/CREAR_DOCUMENTACION/registrar.py
@@ -3,24 +3,29 @@ from USER import users
 from USER import contrasena
 
 def registrar(validar_cuantos_datos=True):
-    lista_datos=[]
-    if validar_cuantos_datos:
-        cuantos_datos = int(input("ingrese la cantidad de datos que ingresara: "))
-    else:
-        cuantos_datos = 1  # Solo se registrara un solo usuario por login 
-    for i in range(cuantos_datos):
-        nombre=input("ingrese su nombre: ").title()
-        correo=input("ingrese su correo: ")
-        c_encriptada= contrasena.contrasena_encriptada()
-        tipo_de_usuario="Regular"
-        fecha = datetime.now()# Obteniendo Fecha Actual
-        fecha_actual = fecha.strftime("%Y-%m-%d")
-        hora_actual = fecha.strftime("%H:%M:%S")    
-        # sUsuario=users.Users(correo,contrasena.contrasena_encriptada())
-        tUsuario = users.TypeOfUsers(nombre, correo, c_encriptada, fecha_actual, hora_actual, tipo_de_usuario)
-        # print("hola mundo") #TEST
-        lista_datos.append([tUsuario.user,tUsuario.email,tUsuario.password, tUsuario.type_of_user,tUsuario.date,tUsuario.hour])
-    return lista_datos,contrasena.desencriptar_clave.desencriptar(c_encriptada) 
+    while True:
+                try:    
+                    lista_datos=[]
+                    if validar_cuantos_datos:
+                        cuantos_datos = int(input("ingrese la cantidad de datos que ingresara: "))
+                    else:
+                        cuantos_datos = 1  # Solo se registrara un solo usuario por login 
+                    for i in range(cuantos_datos):
+                        nombre=input("ingrese su nombre: ").title()
+                        correo=input("ingrese su correo: ")
+                        c_encriptada= contrasena.contrasena_encriptada()
+                        tipo_de_usuario="Regular"
+                        fecha = datetime.now()# Obteniendo Fecha Actual
+                        fecha_actual = fecha.strftime("%Y-%m-%d")
+                        hora_actual = fecha.strftime("%H:%M:%S")    
+                        # sUsuario=users.Users(correo,contrasena.contrasena_encriptada())
+                        tUsuario = users.TypeOfUsers(nombre, correo, c_encriptada, fecha_actual, hora_actual, tipo_de_usuario)
+                        # print("hola mundo") #TEST
+                        lista_datos.append([tUsuario.user,tUsuario.email,tUsuario.password, tUsuario.type_of_user,tUsuario.date,tUsuario.hour])
+                    return lista_datos,contrasena.desencriptar_clave.desencriptar(c_encriptada)
+                except ValueError as e:   
+                    print("⚠️ Error: Ingrese un número válido.")
+                    print(f"Error: {type(e).__name__}")      
 
 # func_registrar = registrar()
 
